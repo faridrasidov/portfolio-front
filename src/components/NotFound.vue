932d514856ba08a0bedfa45f9fd50324f643d005
FaridRasidov <ftm5pv70@duck.com>
2024-08-18 20:15:44 +0330

    Edit: NotFound And NotFound Css Codes

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>notfound</div>
+    <div class="error-section">
+      <div class="error-content">
+        <div class="error-left">
+          <img alt="404-image" class="error-image" src="/404-image.svg"/>
+        </div>
+        <div class="error-right">
+          <h1 class="error-right-header">Lost in space?</h1>
+          <p class="error-left-description">
+            It looks like that page doesn't exist <br/>
+            please check the URL and try again.
+          </p>
+          <RouterLink className="error-button" to="/">
+            Go Back Home
+          </RouterLink>
+        </div>
+      </div>
+    </div>
 </template>
 
-<script>
-
+<script setup>
 </script>
