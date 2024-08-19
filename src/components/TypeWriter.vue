a679182991e85cd9e3b56a8e26e4edecb3343b1c
FaridRasidov <ftm5pv70@duck.com>
2024-08-19 22:18:36 +0330

    Edit: Typewriter Component Written

--- src/components/TypeWriter.vue
+++ src/components/TypeWriter.vue
@@ -1,7 +1,37 @@
 <template>
-  <div>typewriter</div>
+  <div ref="typewriter"></div>
 </template>
 
 <script>
+import Typewriter from "typewriter-effect/dist/core";
 
+export default {
+  name: "Typewriter",
+  props: {
+    strings: {
+      type: Array,
+      required: true,
+    },
+    autoStart: {
+      type: Boolean,
+      default: true,
+    },
+    loop: {
+      type: Boolean,
+      default: true,
+    },
+    deleteSpeed: {
+      type: Number,
+      default: 50,
+    },
+  },
+  mounted() {
+    new Typewriter(this.$refs.typewriter, {
+      strings: this.strings,
+      autoStart: this.autoStart,
+      loop: this.loop,
+      deleteSpeed: this.deleteSpeed,
+    });
+  },
+};
 </script>
